4ffa2729bcf44b9a1b7e9568caedb2d8fd9f81c7
rdeepak2002 <rdeepak2002@gmail.com>
2021-05-23 15:08:58 -0700

    yo

--- src/components/device.component.js
+++ src/components/device.component.js
@@ -1,23 +1,46 @@
 import React, { Component } from "react";
 
+import socketIOClient from "socket.io-client";
+
 import UserService from "../services/user.service";
+import AuthService from "../services/auth.service";
+
+import qs from "qs";
+
+const ENDPOINT = "http://localhost:5000";
 
 export default class Device extends Component {
   constructor(props) {
     super(props);
 
+    const id = qs.parse(this.props.location.search, { ignoreQueryPrefix: true }).id;
+
     this.state = {
+    currentUser: undefined,
       content: "",
-      error: false
+      error: false,
+      deviceId: id,
+      device: undefined
     };
   }
 
   componentDidMount() {
     UserService.getUserBoard().then(
         response => {
-          this.setState({
-            content: response.data
-          });
+            const currentUser = AuthService.getCurrentUser();
+
+            this.setState({
+                content: response.data,
+                currentUser: currentUser
+            }, ()=>{
+                const socket = socketIOClient(ENDPOINT);
+
+                socket.on("iotMessage", data => {
+                    if(data.owner === this.state.currentUser.id && data.deviceId === this.state.deviceId) {
+                        this.setState({device: data});
+                    }
+                });
+            });
         },
         error => {
           this.setState({
@@ -47,10 +70,11 @@ export default class Device extends Component {
 
             {!this.state.error && (
                 <div>
-                    <div>{this.state.content}</div>
-                    <div>
-                        <div>yay</div>
-                    </div>
+                    {!this.state.device ? (
+                        <div>Loading Device...</div>
+                    ) : (
+                        <div>{this.state.device.deviceId}</div>
+                    )}
                 </div>
             )}
         </header>
