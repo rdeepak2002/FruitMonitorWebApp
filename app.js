8716af7678c553bc1c34c34678e40d7916ce5f1f
Deepak Ramalingam <rdeepak2002@gmail.com>
2021-05-24 01:43:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
+const host = '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -10,4 +11,4 @@ app.get('*', function(req, res) {
     res.sendFile('index.html', {root: path.join(__dirname, 'build')});
 });
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
+app.listen(port, host, () => console.log(`App is live on port ${port}!`))
