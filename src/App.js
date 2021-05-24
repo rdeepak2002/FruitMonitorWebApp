d5f9042d29802e6105fe560e30a1a90854c8655d
rdeepak2002 <rdeepak2002@gmail.com>
2021-05-23 17:49:02 -0700

    galaxy brain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -12,7 +12,7 @@ import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 import Device from "./components/device.component";
 
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +20,8 @@ class App extends Component {
     this.logOut = this.logOut.bind(this);
 
     this.state = {
-      currentUser: undefined
+      currentUser: undefined,
+      goToRoot: false
     };
   }
 
@@ -36,77 +37,74 @@ class App extends Component {
 
   logOut() {
     AuthService.logout();
+    this.setState({goToRoot: true});
+    window.location.reload();
   }
 
   render() {
     const { currentUser } = this.state;
 
     return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            FruitVision
-          </Link>
-          <div className="navbar-nav mr-auto">
+        <Router>
+        {this.state.goToRoot && (
+            <Redirect to="/"></Redirect>
+        )}
 
-            {!currentUser && (
-                <li className="nav-item">
-                    <Link to={"/home"} className="nav-link">
-                        Home
-                    </Link>
-                </li>
-            )}
+        <div>
+            <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
+                <div className="container">
+                    <Navbar.Brand href="/">FruitVision</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            {currentUser && (
-              <li className="nav-item">
-                <Link to={"/user"} className="nav-link">
-                  Devices
-                </Link>
-              </li>
-            )}
-          </div>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            {currentUser && (
+                                <NavLink to={"/user"} className="nav-link" activeClassName="active">Devices</NavLink>
+                            )}
+                        </Nav>
+                        <Nav>
+                            {currentUser && (
+                                <NavLink to={"/profile"} className="nav-link" activeClassName="active">My Account</NavLink>
+                            )}
 
-          {currentUser ? (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                  Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
-                  Log Out
-                </a>
-              </li>
+                            {currentUser && (
+                                <NavLink to={"/login"} className="nav-link" activeClassName="active" onClick={this.logOut}>Log Out</NavLink>
+                            )}
+
+                            {!currentUser && (
+                                <NavLink to={"/login"} className="nav-link" activeClassName="active">Login</NavLink>
+                            )}
+
+                            {!currentUser && (
+                                <NavLink to={"/register"} className="nav-link" activeClassName="active">Sign Up</NavLink>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
+
+            <div>
+                <Switch>
+                    <Route exact path={["/", "/home"]} component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route path="/user" component={BoardUser} />
+                    <Route path="/device" component={Device} />
+                    
+                    {(currentUser) && (
+                        <Route path='*'>
+                            <Redirect to="/user" />
+                        </Route>
+                    )}
+
+                    {(!currentUser) && (
+                        <Redirect to="/home" />
+                    )}
+                </Switch>
             </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Sign Up
-                </Link>
-              </li>
-            </div>
-          )}
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/home"]} component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/profile" component={Profile} />
-            <Route path="/user" component={BoardUser} />
-            <Route path="/device" component={Device} />
-          </Switch>
         </div>
-      </div>
+    </Router>
     );
   }
 }
